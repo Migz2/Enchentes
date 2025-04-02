553a952d812e13d83b5cbbbd6c66d0c0df35140d
matheus-asilva <ms.asilvas1@gmail.com>
2025-04-02 00:03:39 -0300

    cria modulo de gerar dataset de treino

---
+++ data/preparar_dados/dataset.py
@@ -0,0 +1,9 @@
+# TODO: Preparar o dataset para o modelo de machine learning
+# 1. Carregar os dados da API (start_date, end_date)
+# 2. Carregar os dados do Web Scraping (start_date, end_date)
+# 3. Merge dos dados
+# 4. Feature Engineering
+# 5. Criar o dataset
+
+def create_dataset(start_date="2020-01-01", end_date="2025-04-01"):
+    pass
